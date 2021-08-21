96880121a2c208ac173f4bfb13e1ac3f6667c67b
Charles Mutagorama <c.mutagorama@irembo.com>
2021-08-21 18:11:29 +0200

    adjusted layout

--- components/Layout.js
+++ components/Layout.js
@@ -5,8 +5,12 @@ function Layout({ children }) {
 	return (
 		<div className="flex flex-col h-screen">
 			<Nav />
-			
-			<main className="p-4 flex-grow">{children}</main>
+
+			<main className="flex-grow">
+				<div className="max-w-7xl mx-auto p-4">
+					{children}
+				</div>
+			</main>
 
 			<Footer />
 		</div>
