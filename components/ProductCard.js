d8f3317736ad01291e27f4284247d089c1074898
Charles Mutagorama <c.mutagorama@irembo.com>
2021-08-25 02:14:31 +0200

    removed static data

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,25 +1,25 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useState } from 'react';
-import pic from '../public/shoes-1.jpg';
 
-const ProductCard = ({ }) => {
+const ProductCard = ({ product }) => {
 	const [sBtn, showBtn] = useState(false);
+
 	return (
-		<Link href="/t/id">
+		<Link href={{ pathname: `/t/${product.slug}`, query: { v: product.id } }} passHref>
 			<div className="max-w-sm rounded overflow-hidden hover:cursor-pointer" onMouseEnter={() => showBtn(true)} onMouseLeave={() => showBtn(false)}>
 				<div className="w-full">
-					<Image src={pic} alt="Mountain"/>
+					<Image src={product.imagePreview} height="500" width="400" alt="Mountain"/>
 				</div>
 				<div className="px-6 py-4">
 					{
 						!sBtn ?
 							<div className="text-center">
-								<p>ACG Air Mowabb</p>
+								<p>{product.name}</p>
 								<p className="text-gray-700 text-base">Rattan Birch</p>
 							</div> :
 							<div className="text-center">
-								<button className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full">Notify me</button>
+								<button className="bg-gray-900 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full">Buy</button>
 							</div>
 					}
 				</div>
