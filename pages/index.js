1c11be868785d01f66131c2178295f0c3e77e563
Charles Mutagorama <c.mutagorama@irembo.com>
2021-08-25 02:17:59 +0200

    removed products on home page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
-import ProductCard from '../components/ProductCard';
-
 export default function Home() {
-	const products = [1, 2, 3, 4, 5, 6];
-
 	return (
-		<div className="grid lg:gap-x-8 lg:gap-y-4 lg:grid-cols-3 md:gap-6 md:grid-cols-1">
-			{
-				products.map((p, index) => <ProductCard key={index}/>)
-			}
+		<div className="grid-columns">
+			Home page
 		</div>
-  )
+	);
 }
