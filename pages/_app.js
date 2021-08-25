7502b1108cf762e53e8fe596c5fdf36a9753919f
Charles Mutagorama <c.mutagorama@irembo.com>
2021-08-25 02:18:23 +0200

    wrapped app in query client provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import Layout from '../components/Layout';
+import NextNProgress from '../components/Progressbar';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
+	const queryClient = new QueryClient();
+
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<QueryClientProvider client={queryClient}>
+			<NextNProgress />
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</QueryClientProvider>
 	);
 }
 
