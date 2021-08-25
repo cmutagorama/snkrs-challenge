1d1df5b6c72660241572ad2b40881ae4966a8632
Charles Mutagorama <c.mutagorama@irembo.com>
2021-08-25 02:30:24 +0200

    replaced class to className property

--- components/Skeleton.js
+++ components/Skeleton.js
@@ -1,11 +1,11 @@
 const Skeleton = () => (
-	<div class="border border-gray-200 shadow rounded-md p-4 w-full mx-auto">
-		<div class="animate-pulse flex space-x-4">
-			<div class="flex-1 space-y-8 py-1">
-				<div class="h-4 bg-gray-300 rounded w-3/4"></div>
-				<div class="space-y-8">
-					<div class="h-4 bg-gray-300 rounded"></div>
-					<div class="h-4 bg-gray-300 rounded w-5/6"></div>
+	<div className="border border-gray-200 shadow rounded-md p-4 w-full mx-auto">
+		<div className="animate-pulse flex space-x-4">
+			<div className="flex-1 space-y-8 py-1">
+				<div className="h-4 bg-gray-300 rounded w-3/4"></div>
+				<div className="space-y-8">
+					<div className="h-4 bg-gray-300 rounded"></div>
+					<div className="h-4 bg-gray-300 rounded w-5/6"></div>
 				</div>
 			</div>
 		</div>
