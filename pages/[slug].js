399efcdd50eff79fdf63fe4e24f13ccd7be79d98
Charles Mutagorama <c.mutagorama@irembo.com>
2021-08-25 03:01:58 +0200

    removed hooks in condition statement

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { useQuery } from 'react-query';
+import { NotAvailable } from '../components/NotAvailable';
 import Pagination from '../components/Pagination';
 import ProductList from '../components/ProductList';
 import Skeleton from '../components/Skeleton';
@@ -13,51 +14,50 @@ function SlugPage() {
 	const router = useRouter();
 	const { s: queryParam } = router.query;
 
-	if (queryParam === 'in-stock') {
-		const [currentPage, setCurrentPage] = useState(1);
-		const { data: products, isLoading, error } = useQuery('getProducts', getProducts);
-		
-		if (isLoading) {
-			return (
-				<div className="grid-columns">
-					<Skeleton />
-					<Skeleton />
-					<Skeleton />
-					<Skeleton />
-					<Skeleton />
-					<Skeleton />
-				</div>
-			)
-		};
-		if (error) return <Error statusCode={error.message} />;
-
-		const firstPageIndex = (currentPage - 1) * pageSize;
-		const lastPageIndex = firstPageIndex + pageSize;
-		const paginatedProducts = products.slice(firstPageIndex, lastPageIndex);
-
+	const [currentPage, setCurrentPage] = useState(1);
+	const { data: products, isLoading, error } = useQuery('getProducts', getProducts);
+	
+	if (isLoading) {
 		return (
-			<>
-				{ products.length > 0 ? (
-					<>
-						<ProductList products={paginatedProducts}/>
-						<Pagination
-							currentPage={currentPage}
-							totalCount={products.length}
-							pageSize={pageSize}
-							onPageChange={page => setCurrentPage(page)}
-						/>
-					</>
+			<div className="grid-columns">
+				<Skeleton />
+				<Skeleton />
+				<Skeleton />
+				<Skeleton />
+				<Skeleton />
+				<Skeleton />
+			</div>
+		)
+	};
+	if (error) return <Error statusCode={error.message} />;
+
+	const firstPageIndex = (currentPage - 1) * pageSize;
+	const lastPageIndex = firstPageIndex + pageSize;
+	const paginatedProducts = products.slice(firstPageIndex, lastPageIndex);
+
+	return (
+		<>
+			{ queryParam === 'in-stock' && (
+					products.length > 0 ? (
+						<>
+							<ProductList products={paginatedProducts} />
+							<Pagination
+								currentPage={currentPage}
+								totalCount={products.length}
+								pageSize={pageSize}
+								onPageChange={page => setCurrentPage(page)}
+							/>
+						</>
 					) : (
-						<div className="flex justify-center">
-							<h3 className="text-xl font-semibold">No product available at the moment.</h3>
-						</div>
+						<NotAvailable/>
 					)
-				}
-			</>
-		);
-	} else {
-		return <h4>Upcoming items</h4>
-	}
+				)
+			}
+			{
+				queryParam === 'upcoming' && <h4>Upcoming items</h4>
+			}
+		</>
+	);
 }
 
 export default SlugPage;
