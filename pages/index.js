cb3f2613df7bf2143f08c9fdd61af7786ce44e5d
Charles Mutagorama <c.mutagorama@irembo.com>
2021-08-26 12:11:44 +0200

    added mocked products

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,56 @@
+import ProductList from "../components/ProductList";
+import shoe1 from '../public/images/s-1.jpg';
+import shoe2 from '../public/images/s-2.jpg';
+import shoe3 from '../public/images/s-3.jpg';
+import shoe4 from '../public/images/s-4.jpg';
+import shoe5 from '../public/images/s-5.jpg';
+import shoe6 from '../public/images/s-6.jpg';
+
 export default function Home() {
-	return (
-		<div className="grid-columns">
-			Home page
-		</div>
-	);
+	const prods = [
+		{
+			id: 1,
+			slug: "air-jordan-low",
+			imagePreview: shoe1,
+			name: "Air Jordan 1 Low",
+			state: true
+		},
+		{
+			id: 2,
+			slug: "big-kids-dunk-low",
+			imagePreview: shoe2,
+			name: "Big Kids' Dunk Low",
+			state: true
+		},
+		{
+			id: 3,
+			slug: "air-huarache-low",
+			imagePreview: shoe3,
+			name: "Air Huarache",
+			state: true
+		},
+		{
+			id: 4,
+			slug: "air-jordan-four",
+			imagePreview: shoe4,
+			name: "Air Jordan 4",
+			state: true
+		},
+		{
+			id: 5,
+			slug: "",
+			imagePreview: shoe5,
+			name: "",
+			state: true
+		},
+		{
+			id: 6,
+			slug: "ldwaffle-sacal-fragment",
+			imagePreview: shoe6,
+			name: "LDWaffle x sacal x Fragment",
+			state: true
+		},
+	];
+
+	return (<ProductList products={prods} />);
 }
