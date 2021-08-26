85773b85f399382db28dbbaccf5dfed3333fa6e4
Charles Mutagorama <c.mutagorama@irembo.com>
2021-08-26 12:08:42 +0200

    preloading fonts

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,42 @@
+import Head from "next/head";
 import Footer from "./Footer";
 import Nav from "./Nav";
 
 function Layout({ children }) {
 	return (
-		<div className="flex flex-col h-screen">
-			<Nav />
+		<>
+			<Head>
+				<link
+					rel="preload"
+					href="/fonts/GTWalsheimProRegular.ttf"
+					as="font"
+					crossOrigin=""
+				/>
+				<link
+					rel="preload"
+					href="/fonts/GTWalsheimProMedium.ttf"
+					as="font"
+					crossOrigin=""
+				/>
+				<link
+					rel="preload"
+					href="/fonts/GTWalsheimProBold.ttf"
+					as="font"
+					crossOrigin=""
+				/>
+      </Head>
+			<div className="flex flex-col h-screen">
+				<Nav />
 
-			<main className="flex-grow">
-				<div className="max-w-7xl mx-auto md:p-4 sm:p-0">
-					{children}
-				</div>
-			</main>
+				<main className="flex-grow">
+					<div className="max-w-7xl mx-auto md:p-4 sm:p-0">
+						{children}
+					</div>
+				</main>
 
-			<Footer />
-		</div>
+				<Footer />
+			</div>
+		</>
 	);
 }
 
